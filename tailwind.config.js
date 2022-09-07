8b612a6706a518eee49fc3e55dd3b392688fae5f
박용진 <insight.emoket@gmail.com>
2022-09-07 11:03:06 +0900

    header section blur bg effects added close #4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -30px) scale(1.3)',
+          },
+          '66%': {
+            transform: 'translate(-30px, 30px) scale(0.8)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
